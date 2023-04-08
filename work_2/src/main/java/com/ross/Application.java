28cf94ba7e03ff1cfa8fdd0d0e0ae0a226f372e8
jqdelove <jqdelovevip@gmail.com>
2023-04-08 22:09:31 +0800

    第二次提交

---
+++ work_2/src/main/java/com/ross/Application.java
@@ -0,0 +1,16 @@
+package com.ross;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: jinqi
+ * @create: 2023-04-08 22:00
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
